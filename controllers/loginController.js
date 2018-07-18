5e6f98f87dfec8bd164dbe01c709f18ab1527f68
Ozan Alpay <ozanalpay@yandex.com.tr>
2018-07-18 19:38:11 +0200

    Rework on Login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,26 +2,24 @@ const path = require('path');
 const axios = require('axios');
 var bodyParser = require('body-parser');
 
+
+
+
 exports.get_login_page = function(req, res) {
 	res.render(path.join(__dirname, "../public/pages/login"));
 };
 
 exports.login_request= function(req, res) {
 	
-	axios.get('http://localhost:3000/api/system/identities').then(function(response)
-	{
-		for(var i=0; i < response.data.length; i++) {
-			
-			if(req.body.username == response.data[i].name) {
-				
-				var id = response.data[i].participant.split('#')[1];
-
-				create_session_with_user_credentials(id, req, res)
-				break;
-			}
+	axios.get('http://localhost:3000/api/system/identities/'+ req.body.password).then(function(response) {
+		if(response.data.name == req.body.username) {
+			var id = response.data.participant.split("#")[1];
+			create_session_with_user_credentials(id, req, res)
+		} else {
+			console.log("Incorrect Name");
 		}
-	}).catch(function(error){
-		console.log(error);
+	}).catch(function(error) {
+		console.log("Incorrect Identity ID :(");
 	});
 };
 
@@ -34,7 +32,6 @@ async function create_session_with_user_credentials(id, req, res) {
 		req.session.user=response.data;
 		req.session.user.oe=req.session.user.oe.split("#")[1];
 		app.set('USER', req.session.user);
-
 		res.redirect("/ics/");
 	}).catch(function(error) {
 		console.log(error);
