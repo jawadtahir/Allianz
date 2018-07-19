7579f1c454c5baea6ec3fad973b335b1c25b0307
Haris Magdalinos <chmagdalinos@gmail.com>
2018-07-19 02:59:42 +0200

    Handling bill & Owning Bill details.

--- controllers/hbillController.js
+++ controllers/hbillController.js
@@ -1,3 +1,5 @@
+const BusinessNetworkDefinition = require('composer-client');
+
 const path = require('path');
 const axios = require('axios');
 const BILL_API_ENDPOINT = 'http://localhost:3000/api/Bill';
@@ -10,16 +12,31 @@ exports.OEhbillList = function(req,res) {
         axios.get(BILL_API_ENDPOINT + filter)
         .then(response => {
             hbills = response.data;
+            for(var i = 0; i < hbills.length;i++){
+                var date = hbills[i].dueDate.toString().split(':')
+                var dateHour = date[0];
+                var min = date[1];
+                var sec = date[2];
+                var temp = dateHour.split('T');
+                date = temp[0];
+                var hour = temp[1];
+                temp = date.split('-');
+                var year = temp[0];
+                var month = temp[1];
+                var day = temp[2];
+                sec = sec.split('.')[0];
+                hbills[i].dueDate = day + '/' + month + '/' + year + ' ' + hour + ':' + min;
+                hbills[i].ooe = hbills[i].ooe.split('#')[1];
+            }
             res.render(path.join(__dirname, "../public/pages/hbills"), {
                 hbills: hbills,
-                user:app.get('USER')
+                user:app.get('USER'),
             });
         })
         .catch(error => {
             console.log(error);
         });
     }
-
 }
 
 function creatFilterQuery(oe) {
--- controllers/obillController.js
+++ controllers/obillController.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const axios = require('axios');
+NAMESPACE = "de.tum.allianz.ics.";
+RESOURCE = "resource:de.tum.allianz.ics.";
+
 /**
  * {"where":{"and":[{"or":[{"status":"PENDING"},{"status":"SHOULDAUTH"}]},{"ooe":"resource:de.tum.allianz.ics.OE#OE"}]}}
  */
@@ -39,9 +42,51 @@ function getBills(authList, pendingList, req, res) {
             
         });
     }
-    
+}
+
+exports.editBill = function editBill(req, res) {
+    axios.get(BILL_API_ENDPOINT + '/' + req.params.id).then(function (response) {
+            var bill = response.data;
+            var transactionData = new Object();
+            transactionData.$class = NAMESPACE + 'Bill';
+            transactionData.ooe = bill.ooe;
+            transactionData.hoe = bill.hoe;
+            transactionData.billId = bill.billId;
+            transactionData.claims = bill.claims;
+            transactionData.status = bill.status;
+            transactionData.totalAmount = bill.totalAmount;
+            transactionData.dueDate = req.body.dueDate;
+            transactionData.latePenality = bill.latePenality;
+            transactionData.totalOutstanding = (parseFloat(bill.totalOutstanding) - parseFloat(bill.handlingFee)).toString();
+            var temp = transactionData.dueDate.split('/');
+            var year = temp[2];
+            var month = temp[0];
+            var day = temp[1];
+            transactionData.dueDate = new Date(parseInt(year), parseInt(month), parseInt(day), 23, 59, 59);
+            transactionData.handlingFee = req.body.handlingFee;
+            transactionData.totalOutstanding = (parseFloat(transactionData.totalOutstanding) + parseFloat(transactionData.handlingFee)).toString();
+            if(bill.authorizor)
+            {
+                transactionData.authorizor = bill.authorizor;
+            }
+            if(bill.payer)
+            {
+                transactionData.payer = bill.payer;
+            }
+            if(transactionData.authDate)
+            {
+                transactionData.authDate = bill.authDate;
+            }
+            axios.put("http://localhost:3000/api/Bill/"+ bill.billId.toString(), transactionData).then(function(response){
+                //location.reload(); @Jawad I commented it, since it breaks element-hiding, but if neccessary i can uncomment it again and find another way to fix it.
+                res.redirect("/ics/obills");
+            }).catch(function(error){
+                res.redirect("/ics/obillhist/"+bill.billId.toString());
+            });
+    });
 
 }
+
 
 function createFilter(oe) {
     if (oe != undefined){
